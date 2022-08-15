bea90f289f7db4efa4a3a44eeb06edb312a8514f
glcnk <108413143+aysesera@users.noreply.github.com>
2022-08-15 21:58:04 +0300

    Update Day2GitGithub.java

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -14,5 +14,6 @@ göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak
             git push -u origin master
 Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
 kullanırız
+Not gşthub ogrendim
      */
 }
